fbf76ad7d3783b8e821c09a52af4cfe23356735a
ryo1116 <ryo1103yosi@gmail.com>
2013-02-28 10:16:25 +0900

    first commit

---
+++ YouTube/YouTube/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使用して変更できます。  
+    'コード エディタを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.Panel2 = New System.Windows.Forms.Panel
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.WebBrowser1)
+        Me.Panel1.Location = New System.Drawing.Point(10, 10)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(500, 300)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.PictureBox2)
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Location = New System.Drawing.Point(21, 14)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(453, 283)
+        Me.Panel2.TabIndex = 2
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApplication1.My.Resources.Resources.youtube_logo
+        Me.PictureBox2.Location = New System.Drawing.Point(133, 184)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(190, 83)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.kobeko
+        Me.PictureBox1.Location = New System.Drawing.Point(18, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(411, 155)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(500, 300)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 324)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 24)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "殿様バッタ編"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(140, 324)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 24)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "ETロボコン転倒編"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(269, 324)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 24)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "AKB48"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(115, 360)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(306, 19)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(389, 324)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(95, 24)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "One World"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(429, 357)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(55, 24)
+        Me.Button5.TabIndex = 1
+        Me.Button5.Text = "検索"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 362)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 14)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "キーワード検索："
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 393)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Youtube Player"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ YouTube/YouTube/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Xml
+Imports System.Net
+Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' 神戸電子　殿様バッタ編
+        WebBrowser1.Navigate("http://www.youtube.com/v/ypuWyPRRy9E&autoplay=1")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ' ETロボコン2011 CS大会 インコース転倒シーン
+        WebBrowser1.Navigate("http://www.youtube.com/v/p7pIJ8UdMR8&autoplay=1")
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ' AKB48 ヘビーローテーション
+        WebBrowser1.Navigate("http://www.youtube.com/v/lkHlnWFnA0c&autoplay=1")
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        ' One World Baton Relay in JAPAN 2012 兵庫県 神戸電子専門学校にて
+        WebBrowser1.Navigate("http://www.youtube.com/v/QUgmNphhuqg&autoplay=1")
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        ' 入力されたキーワードによりYoutube動画を検索し、表示させる
+        Dim videoID As String = "ypuWyPRRy9E"
+        Dim keyword As String = "神戸電子専門学校 殿様バッタ"
+
+        If TextBox1.Text <> "" Or TextBox1.Text <> Nothing Then
+            ' ここでテキストボックスに入力されたキーワードを取得する
+            keyword = TextBox1.Text
+        End If
+
+        ' キーワードを使ってYoutubeから動画を1件検索
+        Dim search As String = "http://gdata.youtube.com/feeds/api/videos?vq=" + keyword + "&orderby=relevance_lang_ja&max-results=1"
+        Dim XML As String = GetXML(search)
+
+        ' XMLデータを取り扱うための準備。ルート要素の取得
+        Dim Doc As New XmlDocument
+        Doc.LoadXml(XML)
+        Dim root As XmlNode = Doc.DocumentElement
+
+        ' Add the namespace.（名前空間）
+        Dim nsmgr As New XmlNamespaceManager(Doc.NameTable)
+        nsmgr.AddNamespace("ns", "http://www.w3.org/2005/Atom")
+
+        ' URLを取り出す
+        Dim node As XmlNode = root.SelectSingleNode("descendant::ns:entry/ns:id", nsmgr)
+        videoID = node.InnerXml
+
+        ' 余分なデータを除いたIDを取り出す
+        Dim videoParam As String = videoID.Substring(videoID.LastIndexOf("/") + 1)
+
+        ' 動画用URLを生成する
+        Dim videoURL As String = "http://www.youtube.com/v/" + videoParam + "&autoplay=1"
+        ' MessageBox.Show(videoURL)
+
+        ' ここで画面に表示する
+        WebBrowser1.Navigate(videoURL)
+
+    End Sub
+
+    Function GetXML(ByVal URL As String) As String
+        Dim Request As HttpWebRequest = HttpWebRequest.Create(URL)
+        Dim sr As New StreamReader(Request.GetResponse.GetResponseStream)
+
+        Request.Method = "POST"
+        GetXML = sr.ReadToEnd
+        sr.Close()
+        Request.GetResponse.Close()
+    End Function
+
+    Private Sub ClearImage() Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click
+        'PictureBox1.Visible = False
+        Panel2.Visible = False
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        On Error Resume Next
+
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Call Button5_Click(sender, e)
+            Panel2.Visible = False
+        End If
+    End Sub
+End Class
